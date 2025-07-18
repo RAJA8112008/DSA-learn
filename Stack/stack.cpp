695405ae7e9624bc0d0370fb5909c54208c37f4a
Raja <kraj9380286@gmail.com>
2025-07-18 11:37:27 +0530

    commited

--- Stack/stack.cpp
+++ Stack/stack.cpp
@@ -1,92 +1,33 @@
 #include<iostream>
 #include<stack>
 using namespace std;
-// class stack{
-//   public:
-//   int *arr;
-//   int size;
-//   int top1;
-//   int top2;
-//   //constrocture
-//   stack(int size){
-//    arr=new int[size];
-//    this->size=size;
-//    top1=-1;
-//    top2=size;
-//   }
-//   //functions
-//   void push1(int data){
-//      if(top2-top1==1){
-//       cout<<"Overflow"<<endl;
-//      }else{
-//       top1++;
-//       arr[top1]=data;
-//      }
-//   }
-//   void pop1(){
-//     if(top1==-1){
-//       cout<<"underflow"<<endl;
-//     }else{
-//       top1--;
-//     }
-//   }
-//     void push2(int data){
-//  if(top2-top1==1){
-//       cout<<"Overflow"<<endl;
-//      }else{
-//       top2--;
-//       arr[top2]=data;
-//      }
-//   }
-  
-//   void pop2(){
-//     if(top2==size){
-//       cout<<"Underflow"<<endl;
-//     }else{
-//       top2++;
-//     }
-//   }
-//     void print(){
-//       cout<<endl;
-//       cout<<"top1"<<top1<<endl;
-//       cout<<"top2"<<top2<<endl;
-//       for(int i=0;i<size;i++){
-//         cout<<arr[i]<<" ";
-//       }cout<<endl;
-//     }
-//   };
-// int main(){
-// stack s(10);
-// s.push1(10);
-// s.print();
-// s.push1(20);
-// s.print();
-// s.push1(30);
-// s.print();
-// s.push1(40);
-// s.print();
-// s.push1(50);
-// s.print();
 
-// s.push2(100);
-// s.print();
-// s.push2(90);
-// s.print();
-// s.push2(80);
-// s.print();
-// s.push2(70);
-// s.print();
-// s.push2(60);
-// s.print();
-// }
-int main(){ 
-  string str="KUMAR";
-  stack<char>s;
-  for(int i=0;i<str.length();i++){
-    s.push(str[i]);
+
+void midsearch(stack<int>&s,int &totalsize){
+    
+  //base conition
+  if(s.size()==totalsize/2+1){
+    cout<<"Middle elem is:"<<s.top()<<endl;
+    return;
   }
-  while(!s.empty()){
-     cout<<s.top()<<" ";
-     s.pop();
-  }cout<<endl;
+int temp=s.top();
+s.pop();
+   midsearch(s,totalsize);
+   s.push(temp);
+
+}
+int main(){
+  stack<int>s;
+  s.push(10);
+  s.push(20);
+  s.push(30);
+  s.push(40);
+  s.push(50);
+  s.push(60);
+  s.push(70);
+   s.push(80);
+  s.push(90);
+  s.push(100);
+int totalsize=s.size();
+  midsearch(s,totalsize);
 }
