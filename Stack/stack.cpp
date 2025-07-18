1e3812ed8fe0ce293870eeb5c98c0e668d075c44
Raja <kraj9380286@gmail.com>
2025-07-18 09:53:12 +0530

    commited

--- Stack/stack.cpp
+++ Stack/stack.cpp
@@ -1,72 +1,61 @@
 #include<iostream>
+
 using namespace std;
-
-class stack {
-public:
-    int* arr;
-    int size;
-    int top;
-
-    // Constructor
-    stack(int size) {
-        arr = new int[size];
-        this->size = size;
-        top = -1;
+class stack{
+  public:
+  int *arr;
+  int size;
+  int top;
+  stack(int size){
+    arr=new int[size];
+    this->size=size;
+    top=-1;
+  }
+  void push(int data){
+    if(size-top>1){
+      top++;
+      arr[top]=data;
+    }else{
+      cout<<"stack overflow"<<endl;
     }
-
-    // Push data to stack
-    void push(int data) {
-        if (size - top > 1) {
-            top++;
-            arr[top] = data;
-        } else {
-            cout << "Stack overflow" << endl;
-        }
+  }
+    void pop(){
+      if(top==-1){
+       cout<<"stack is underflow"<<endl;
+      }else{
+        top--;
+      }
+  }
+  int getTop(){
+    if(top==-1){
+      cout<<"Empty"<<endl;
+    }else{
+      return arr[top];
     }
-
-    // Pop data from stack
-    void pop() {
-        if (top == -1) {
-            cout << "Stack underflow" << endl;
-        } else {
-            top--;
-        }
+  }
+  int getSize(){
+    return top+1;
+  }
+  bool isEmpty(){
+    if(top==-1){
+      return true;
+    }else{
+      return false;
     }
-
-    // Get top element
-    int getTop() {
-        if (top == -1) {
-            cout << "Empty" << endl;
-            return -1;  // Return -1 to avoid garbage
-        } else {
-            return arr[top];
-        }
-    }
-
-    // Get current size of stack
-    int getSize() {
-        return top + 1;
-    }
-
-    // Check if stack is empty
-    bool isEmpty() {
-        return top == -1;
-    }
+  }
 };
-
-// ✅ Main function
-int main() {
+int main(){
     stack s(10);
     s.push(20);
     s.push(30);
     s.push(40);
     s.push(50);
     s.push(60);
-
-    while (!s.isEmpty()) {
-        cout << s.getTop() << " ";
-        s.pop();
+    while(!s.isEmpty()){
+      cout<<s.getTop()<<" ";
+      s.pop();
     }
-
     return 0;
-}
+  }
+
+  
