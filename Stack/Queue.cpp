ed125f71d2e95bed395f1a028b2013c75db714c6
Raja <kraj9380286@gmail.com>
2025-07-21 15:49:58 +0530

    commited

--- Stack/Queue.cpp
+++ Stack/Queue.cpp
@@ -2,6 +2,18 @@
 #include<queue>
 #include<stack>
 using namespace std;
+
+void recursion(queue<int>&q){
+    if(q.empty()){
+        return;
+    }
+    int element=q.front();
+     q.pop();
+    //calling of the function
+      recursion(q);
+      q.push(element);
+}
+
 void reverseQueue(queue<int>&q){
   stack<int>st;
  while(!q.empty()){
@@ -22,7 +34,8 @@ int main(){
   q.push(4);
   q.push(9);
   q.push(7);
-  reverseQueue(q);
+//   reverseQueue(q);
+recursion(q);
   cout<<"Printing the Queue:"<<endl;
   while(!q.empty()){
     cout<<q.front()<<" ";
