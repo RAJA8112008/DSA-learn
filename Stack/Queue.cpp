63c97135114de5e8f9f15c1a4f9de460bc7a01f7
Raja <kraj9380286@gmail.com>
2025-07-21 21:39:34 +0530

    commited

--- Stack/Queue.cpp
+++ Stack/Queue.cpp
@@ -2,48 +2,83 @@
 #include<queue>
 #include<stack>
 using namespace std;
-void reverseK(queue<int>&q,int k){
-  stack<int>st;
-  int count=0;
+// void reverseK(queue<int>&q,int k){
+//   stack<int>st;
+//   int count=0;
+//   int n=q.size();
+//    if(k==0 ||k>n){
+//     return;
+//    }
+//    while(!q.empty()){
+//     int element=q.front();
+//     q.pop();
+//     st.push(element);
+//     count++;
+//     if(count==k){ 
+//       break;
+//     }
+//    }
+//    //stack to queue
+//       while(!st.empty()){
+//       int element=st.top();
+//       st.pop();
+//       q.push(element);
+//     }
+//     // again enter in queue remaining elements
+//     count=0;
+//     while(!q.empty() && n-k!=0){
+//       int element=q.front();
+//       q.pop();
+//       q.push(element);
+//       count++;
+//       if(count==n-k){
+//         return;
+//       }
+//     }
+// };
+
+ void interleaveQueue(queue<int>&q){
   int n=q.size();
-   if(k==0 ||k>n){
-    return;
-   }
-   while(!q.empty()){
-    int element=q.front();
-    q.pop();
-    st.push(element);
-    count++;
-    if(count==k){ 
+  int k=n/2;
+  int count=0;
+    queue<int>q2;
+    while(!q.empty()){
+     int element=q.front();
+     q.pop();
+     q2.push(element);
+     count++;
+     if(count==k){
       break;
+     }
     }
-   }
-   //stack to queue
-      while(!st.empty()){
-      int element=st.top();
-      st.pop();
-      q.push(element);
-    }
-    // again enter in queue remaining elements
-    count=0;
-    while(!q.empty() && n-k!=0){
-      int element=q.front();
+    //interleave
+    while(!q.empty()&&!q2.empty()){
+      int first=q2.front();
+      q2.pop();
+      q.push(first);
+      int second=q.front();
       q.pop();
-      q.push(element);
-      count++;
-      if(count==n-k){
-        return;
+      q.push(second);
+      if(n&1){
+       int element= q.front();
+       q.pop();
+       q.push(element);
       }
     }
 }
 int main(){
 queue<int>q;
+q.push(1);
+q.push(2);
+q.push(3);
 q.push(4);
 q.push(5);
-q.push(8);
 q.push(6);
-q.push(1);
-reverseK(q,5);
+q.push(7);
+q.push(8);
+// q.push(6);
+// q.push(1);
+interleaveQueue(q);
 cout<<"Printing the queue"<<endl;
 while(!q.empty()){
   cout<<q.front()<<" ";
