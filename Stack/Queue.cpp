1736dc9d51cf8adc1982d83876abfe836c74c232
Raja <kraj9380286@gmail.com>
2025-07-21 09:08:54 +0530

    commited

---
+++ Stack/Queue.cpp
@@ -0,0 +1,63 @@
+#include<iostream>
+using namespace std;
+class Queue{
+    public:
+      int *arr;
+      int size;
+      int front;
+      int rear;
+      //constructor
+    Queue(int size){
+     arr=new int[size];
+     this->size=size;
+     front=0;
+     rear=0;
+    }
+     void push(int data){
+      if(size==rear){
+        cout<<"Queue is full"<<endl;
+      }else{
+         arr[rear]=data;
+         rear++;
+      }
+     }
+     void pop(){
+      if(front==rear){
+         cout<<"Queue is empty"<<endl;
+      }else{
+         arr[front]=-1;
+         front++;
+         if(front==rear){
+            front=0;
+            rear=0;
+         }
+      }
+     }
+     bool isEmpty(){
+        if(front==rear){
+            return true;
+        }else{
+            return false;
+        }
+     }
+     int getSize(){
+        return rear-front;
+     }
+     int getFront(){
+        if(front==rear){
+            cout<<"Queue is Empty"<<endl;
+            return -1;
+        }else{
+            return arr[front];
+        }
+     }
+   };
+   
+int main(){
+   Queue q(10);
+   q.push(10);
+   q.push(15);
+   q.push(20);
+   q.push(25);
+cout<<"Size of Queue:"<<q.getSize()<<endl;
+}
