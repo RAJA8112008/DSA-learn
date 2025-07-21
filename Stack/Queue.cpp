e2cb9da4462fac8d0dbf4a7fbd2e9cb44d5ad52d
Raja <kraj9380286@gmail.com>
2025-07-21 20:42:24 +0530

    commited

--- Stack/Queue.cpp
+++ Stack/Queue.cpp
@@ -2,44 +2,52 @@
 #include<queue>
 #include<stack>
 using namespace std;
-
-void recursion(queue<int>&q){
-    if(q.empty()){
-        return;
-    }
-    int element=q.front();
-     q.pop();
-    //calling of the function
-      recursion(q);
-      q.push(element);
-}
-
-void reverseQueue(queue<int>&q){
+void reverseK(queue<int>&q,int k){
   stack<int>st;
- while(!q.empty()){
-   int element=q.front();
+  int count=0;
+  int n=q.size();
+   if(k==0 ||k>n){
+    return;
+   }
+   while(!q.empty()){
+    int element=q.front();
+    q.pop();
+    st.push(element);
+    count++;
+    if(count==k){ 
+      break;
+    }
+   }
+   //stack to queue
+      while(!st.empty()){
+      int element=st.top();
+      st.pop();
+      q.push(element);
+    }
+    // again enter in queue remaining elements
+    count=0;
+    while(!q.empty()){
+      int element=q.front();
       q.pop();
-      st.push(element);
- }
- while(!st.empty()){
-  int element=st.top();
-  st.pop();
-  q.push(element);
- }
+      q.push(element);
+      count++;
+      if(count==n-k){
+        return;
+      }
+    }
 }
 int main(){
-  queue<int>q;
-  q.push(3);
-  q.push(5);
-  q.push(4);
-  q.push(9);
-  q.push(7);
-//   reverseQueue(q);
-recursion(q);
-  cout<<"Printing the Queue:"<<endl;
-  while(!q.empty()){
-    cout<<q.front()<<" ";
-    q.pop();
-  }
-  cout<<endl;
+queue<int>q;
+q.push(4);
+q.push(5);
+q.push(8);
+q.push(6);
+q.push(1);
+reverseK(q,3);
+cout<<"Printing the queue"<<endl;
+while(!q.empty()){
+  cout<<q.front()<<" ";
+  q.pop();
+}
+cout<<endl;
 }
