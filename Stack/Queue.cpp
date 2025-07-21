a9aeb0f330d65a737708a1ed5dc5144e63aa2e42
Raja <kraj9380286@gmail.com>
2025-07-21 10:56:16 +0530

    commited

--- Stack/Queue.cpp
+++ Stack/Queue.cpp
@@ -1,66 +1,84 @@
 #include<iostream>
 using namespace std;
-class Queue{
+class CirQueue{
     public:
-      int *arr;
-      int size;
-      int front;
-      int rear;
-      //constructor
-    Queue(int size){
-     arr=new int[size];
-     this->size=size;
-     front=0;
-     rear=0;
-    }
-     void push(int data){
-      if(size==rear){
-        cout<<"Queue is full"<<endl;
-      }else{
-         arr[rear]=data;
-         rear++;
-      }
-     }
-     void pop(){
-      if(front==rear){
-         cout<<"Queue is empty"<<endl;
-      }else{
-         arr[front]=-1;
-         front++;
-         if(front==rear){
-            front=0;
+   int *arr;
+   int size;
+   int front;
+   int rear;
+   CirQueue(int size){
+   this->size=size;
+   arr=new int[size];
+      front=-1;
+      rear=-1;
+   }
+   void push(int data){
+        if((rear + 1 == front) ||(front==0 && rear==size-1)){
+            cout<<"Queue is full"<<endl;
+            return;
+        } else if(rear==-1){
             rear=0;
-         }
-      }
-     }
-     bool isEmpty(){
-        if(front==rear){
-            return true;
+            front=0;
+            arr[rear]=data;
+           
+        }else if(rear==size-1 && front!=0){
+              rear=0;
+              arr[rear]=data;
+            
         }else{
-            return false;
+            rear++;
+           arr[rear] =data;
+        //    rear++;
         }
-     }
-     int getSize(){
-        return rear-front;
-     }
-     int getFront(){
-        if(front==rear){
-            cout<<"Queue is Empty"<<endl;
-            return -1;
-        }else{
-            return arr[front];
-        }
-     }
-   };
-   
+   }
+   void pop(){
+    if(front==-1){
+     cout<<"Queue is empty"<<endl;
+     return;
+    }else if(front==rear){
+       arr[front]=-1;
+       front=-1;
+    rear=-1;
+    }else if(front=size-1){
+          arr[front]=-1;
+      front=0;    
+    }else{
+        arr[front]=-1;
+        front++;
+    }
+   }
+   void display() {
+    if (front == -1) {
+        cout << "Queue is empty" << endl;
+        return;
+    }
+
+    cout << "Queue elements: ";
+    int i = front;
+    while (true) {
+        cout << arr[i] << " ";
+        if (i == rear)
+            break;
+
+        // manual wrap around
+        if (i == size - 1)
+            i = 0;
+        else
+            i++;
+    }
+    cout << endl;
+}
+
+};
 int main(){
-   Queue q(10);
-   q.push(10);
-   q.push(15);
-   q.push(20);
-   q.push(25);
-cout<<"Size of Queue:"<<q.getSize()<<endl;
+CirQueue q(10);
+q.push(10);
+q.push(20);
+q.push(30);
+q.push(40);
+q.push(50);
+q.display();
 q.pop();
-cout<<"Size of Queue:"<<q.getSize()<<endl;
-cout<<"Front of Queue:"<<q.getFront()<<endl;
+q.display();
+
 }
