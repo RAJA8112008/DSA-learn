c6f3fd041adbab8bcbd2711b83442584eb77e8b1
Raja <kraj9380286@gmail.com>
2025-07-23 12:25:34 +0530

    commited

--- Stack/Queue.cpp
+++ Stack/Queue.cpp
@@ -2,20 +2,31 @@
 using namespace std;
   
 class node{
+   public:
    int data;
-   node* left=NULL;
-   node* right=NULL;
+   node* left;
+   node* right;
    //constructor
    node(int d){
       this->data=d;
-      this->left=left;
-      this->right=right;
+      this->left=NULL;
+      this->right=NULL;
    }
 };
-node* buildTree(node*data){
-      
-
+node* buildTree(){
+       
+   int d;
+   cin>>d;
+   if(d==-1){
+      return NULL;
+   }
+  node* root=new node(d);
    
+  cout<<"Enter the Left node data:"<<d<<endl;
+  root->left=buildTree();
+cout<<"Enter the Right node data:"<<d<<endl;
+  root->right=buildTree();
+ return root;
    }
 int main(){
    node* root=NULL;
