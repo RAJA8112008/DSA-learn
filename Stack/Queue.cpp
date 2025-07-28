bfbf401f6be441b23b1d29b1c679c002093c151c
Raja <kraj9380286@gmail.com>
2025-07-28 21:57:24 +0530

    commited

--- Stack/Queue.cpp
+++ Stack/Queue.cpp
@@ -1,34 +1,42 @@
-#include<iostream>
-using namespace std;
-  
-class node{
-   public:
-   int data;
-   node* left;
-   node* right;
-   //constructor
-   node(int d){
-      this->data=d;
-      this->left=NULL;
-      this->right=NULL;
-   }
-};
-node* buildTree(){
-       
-   int d;
-   cin>>d;
-   if(d==-1){
-      return NULL;
-   }
-  node* root=new node(d);
-   
-  cout<<"Enter the Left node data:"<<d<<endl;
-  root->left=buildTree();
-cout<<"Enter the Right node data:"<<d<<endl;
-  root->right=buildTree();
- return root;
-   }
-int main(){
-   node* root=NULL;
-   root=buildTree();
-};
+
+void traversRight(TreeNode<int> *root,vector<int>&ans){
+    // base case
+    if((root==NULL)||(root->right==NULL && root->left==NULL))return;
+    if(root->right){
+       ans.push_back(root->data);
+    }else{
+        ans.push_back(root->data);
+    }
+    ans.push_back(root->data);
+}
+  void traverseLeaf(TreeNode<int>*root,vector<int>&ans){
+      //base case
+      if(root==NULL)return;
+      if(root->right ==NULL &&root->left==NULL){
+          ans.push_back(root->data);
+          return;
+      }
+  }
+ void traverseLeft(TreeNode<int> *root,vector<int>&ans){
+    if((root==NULL)||(root->left==NULL && root->right==NULL))return;
+      if(root->left){
+          ans.push_back(root->data);
+      }else{
+          ans.push_back(root->data);
+      }
+      traverseLeft(root,ans);
+     
+ }
+
+vector<int> traverseBoundary(TreeNode<int> *root)
+{
+  vector<int>ans;
+    if(root==NULL)return ans;
+    ans.push_back(root);
+    traversRight(root->right,ans);
+    traversRight(root->left,ans);
+    traverseLeaf(root,ans);
+     traverseLeft(root->left,ans);
+      traverseLeft(root->right,ans);
+      return ans;
+}
