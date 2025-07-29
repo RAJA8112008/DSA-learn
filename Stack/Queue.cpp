9fc6517078e6b04cd83d02576a279cd329bce281
Raja <kraj9380286@gmail.com>
2025-07-29 10:34:14 +0530

    commited

--- Stack/Queue.cpp
+++ Stack/Queue.cpp
@@ -1,42 +1,16 @@
-
-void traversRight(TreeNode<int> *root,vector<int>&ans){
-    // base case
-    if((root==NULL)||(root->right==NULL && root->left==NULL))return;
-    if(root->right){
-       ans.push_back(root->data);
-    }else{
-        ans.push_back(root->data);
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[5]={12,5,7,58,0};
+    for(int i=0;i<5;i++){ 
+        for(int j=i+1;j<5;j++){
+            if(arr[i]>arr[j]){
+                swap(arr[i],arr[j]);
+            }
+        }
     }
-    ans.push_back(root->data);
-}
-  void traverseLeaf(TreeNode<int>*root,vector<int>&ans){
-      //base case
-      if(root==NULL)return;
-      if(root->right ==NULL &&root->left==NULL){
-          ans.push_back(root->data);
-          return;
-      }
-  }
- void traverseLeft(TreeNode<int> *root,vector<int>&ans){
-    if((root==NULL)||(root->left==NULL && root->right==NULL))return;
-      if(root->left){
-          ans.push_back(root->data);
-      }else{
-          ans.push_back(root->data);
-      }
-      traverseLeft(root,ans);
-     
- }
-
-vector<int> traverseBoundary(TreeNode<int> *root)
-{
-  vector<int>ans;
-    if(root==NULL)return ans;
-    ans.push_back(root);
-    traversRight(root->right,ans);
-    traversRight(root->left,ans);
-    traverseLeaf(root,ans);
-     traverseLeft(root->left,ans);
-      traverseLeft(root->right,ans);
-      return ans;
+    //Printing sorted array
+    for(int i=0;i<5;i++){
+        cout<<arr[i]<<" ";
+    }cout<<endl;
 }
