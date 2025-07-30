912d5e699482364941de2f3d45db98d22c6afa2a
Raja <kraj9380286@gmail.com>
2025-07-30 09:24:53 +0530

    commited

--- Stack/Tree.cpp/BST.cpp
+++ Stack/Tree.cpp/BST.cpp
@@ -1,60 +1,16 @@
 #include<iostream>
 using namespace std;
-
- class Node{
-    public:
-      int data;
-      Node* left;
-      Node* right;
-    Node(int data){
-     this->data=data;
-     this->left=NULL;
-     this->right=NULL;
-    }
- };
-Node* InsertBST(Node* root,int data){
-     if(root==NULL){
-        root=new Node(data);
-        return root;
-     }
-     if(root->data>data){
-       root->left= InsertBST(root->left,data);
-     }else{
-        root->right=InsertBST(root->right,data);
-     }
-     return root;
- }
- Node* Insert(Node*root){
+class Node{
     int data;
-    cin >> data;
-    while(data!=-1){
-        root=InsertBST(root,data);
-        cin>>data;
+    Node* left;
+    Node* right;
+//constructor
+    Node(int data){
+      this->data=data;
+      this->left=NULL;
+      this->right=NULL;
     }
-   return root;
- }
-  void InOrderTraversal(Node*root){
-      if(root==NULL)return;
-      //LNR
-      InOrderTraversal(root->left);
-      cout<<root->data<<" "<<endl;
-      InOrderTraversal(root->right);
-  }
-  void preOrder(Node*root){
-    //NLR
-    if(root==NULL)return;
-      cout<<root->data<<" "<<endl;
-      preOrder(root->left);
-      preOrder(root->right);
-  }
-int main(){  
-   Node* root=NULL;
-  cout<<"Enter the values in root/nodes:"<<endl;
-  root=Insert(root);
-cout<<"INorder traversal";
-InOrderTraversal(root);
- cout<<endl;
- cout<<"Preorder traversal";
- preOrder(root);
- cout<<endl;
+};
+int main(){    
+  Node* root=NULL;
 }
