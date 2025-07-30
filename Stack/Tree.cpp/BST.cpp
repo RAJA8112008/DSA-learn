c2ff9ac3ad3670c55148a893dc11371bd2aa38cc
Raja <kraj9380286@gmail.com>
2025-07-30 08:52:51 +0530

    commited

---
+++ Stack/Tree.cpp/BST.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+
+ class Node{
+    public:
+      int data;
+      Node* left;
+      Node* right;
+    Node(int data){
+     this->data=data;
+     this->left=NULL;
+     this->right=NULL;
+    }
+ };
+Node* InsertBST(Node* root,int data){
+     if(root==NULL){
+        root=new Node(data);
+        return root;
+     }
+     if(root->data>data){
+       root->left= InsertBST(root->left,data);
+     }else{
+        root->right=InsertBST(root->right,data);
+     }
+     return root;
+ }
+ Node* Insert(Node*root){
+    int data;
+    cin >> data;
+    while(data!=-1){
+        root=InsertBST(root,data);
+        cin>>data;
+    }
+   return root;
+ }
+int main(){  
+   Node* root=NULL;
+  cout<<"Enter the values in root/nodes:"<<endl;
+  Insert(root);
+ 
+}
