7f5f66c9f31015cf9be573787e7834e9d5f09cde
Raja <kraj9380286@gmail.com>
2025-07-30 10:13:32 +0530

    commited

--- Stack/Tree.cpp/BST.cpp
+++ Stack/Tree.cpp/BST.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 using namespace std;
 class Node{
+    public:
     int data;
     Node* left;
     Node* right;
@@ -11,6 +12,40 @@ class Node{
       this->right=NULL;
     }
 };
+    Node* InsertData(Node* root,int data){
+      if(root==NULL){
+        root=new Node(data);
+        return root;
+      }
+      if(root->data>data){
+        root->left=InsertData(root->left,data);
+      }else{
+        root->right=InsertData(root->right,data);
+      }
+     return root;
+    }
+ Node* takeData(Node* root){
+    int data;
+    cin>>data;
+      while(data!=-1){
+        root=InsertData(root,data);
+        cin>>data;
+      }
+      return root;
+    }
+    void InOrderTravrse(Node* root){
+
+        //LNR
+        if(root==NULL)return;
+        InOrderTravrse(root->left);
+        cout<<root->data<<" "<<endl;
+        InOrderTravrse(root->right);
+    }
 int main(){    
   Node* root=NULL;
+  cout<<"Enter the data:"<<endl;
+  root= takeData(root);
+  cout<<"InO rder";
+  InOrderTravrse(root);
+  cout<<endl;
 }
