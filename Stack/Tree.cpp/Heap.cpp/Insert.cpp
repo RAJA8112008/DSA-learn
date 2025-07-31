cb3a6908a7330263bf99d4e524972fc7a205c0bc
Raja <kraj9380286@gmail.com>
2025-07-31 21:07:37 +0530

    commited

--- Stack/Tree.cpp/Heap.cpp/Insert.cpp
+++ Stack/Tree.cpp/Heap.cpp/Insert.cpp
@@ -3,76 +3,61 @@
 using namespace std;
 
 class MaxHeap {
-public:
+private:
     vector<int> heap;
 
-    // Constructor to build heap from array
-    MaxHeap(vector<int> arr) {
-        heap = arr;
-        buildHeap();
-    }
-
     // Heapify down from index i
-    void heapify(int i, int n) {
+    void heapify(int i) {
+        int n = heap.size();
         int largest = i;
         int left = 2 * i + 1;
         int right = 2 * i + 2;
 
         if (left < n && heap[left] > heap[largest])
             largest = left;
-
         if (right < n && heap[right] > heap[largest])
             largest = right;
 
         if (largest != i) {
             swap(heap[i], heap[largest]);
-            heapify(largest, n);
+            heapify(largest);
         }
     }
 
-    // Build max heap from array
+public:
+    // Constructor: build heap from array
+    MaxHeap(vector<int>& arr) {
+        heap = arr;
+        buildHeap();
+    }
+
+    // Build heap (heapify all non-leaf nodes)
     void buildHeap() {
         int n = heap.size();
         for (int i = n / 2 - 1; i >= 0; i--) {
-            heapify(i, n);
+            heapify(i);
         }
     }
 
-    // Insert element
-    void insert(int val) {
-        heap.push_back(val);
-        int i = heap.size() - 1;
-
-        // Heapify Up
-        while (i > 0) {
-            int parent = (i - 1) / 2;
-            if (heap[parent] < heap[i]) {
-                swap(heap[parent], heap[i]);
-                i = parent;
-            } else {
-                break;
-            }
-        }
-    }
-
-    // Display the heap
+    // Print heap
     void printHeap() {
-        for (int val : heap)
+        for (int val : heap) {
             cout << val << " ";
+        }
         cout << endl;
     }
 };
 
 int main() {
-    vector<int> arr = {10, 20, 15, 30, 40};
+    vector<int> arr = {3, 5, 1, 10, 2, 7};
+
+    cout << "Original array: ";
+    for (int val : arr) cout << val << " ";
+    cout << endl;
+
     MaxHeap h(arr);
 
-    cout << "Heap after building from array: ";
-    h.printHeap();
-
-    h.insert(50);
-    h.insert(5);
-    cout << "Heap after insertions: ";
+    cout << "Max-Heap created: ";
     h.printHeap();
 
     return 0;
