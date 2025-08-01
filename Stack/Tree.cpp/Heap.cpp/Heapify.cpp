ccabfdd940965b7d8d6433d4082a585934b322a8
Raja <kraj9380286@gmail.com>
2025-08-01 12:10:44 +0530

    commited

--- Stack/Tree.cpp/Heap.cpp/Heapify.cpp
+++ Stack/Tree.cpp/Heap.cpp/Heapify.cpp
@@ -1,50 +1,45 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
-
-class MaxHeap {
-private:
-    int heap[100]; // fixed-size heap array
+class Heap{
+    public:
+    int arr[100];
     int size;
-
-public:
-    MaxHeap() {
-        size = 0;
+    //constructor
+    Heap(){
+      //start from index 1
+      arr[0]=-1;
+      size=0;
     }
-
-    void insert(int value) {
-        heap[size] = value;
-        int index = size;
-        size++;
-
-        // Heapify up (no separate heapify function)
-        while (index > 0) {
-            int parent = (index - 1) / 2;
-            if (heap[parent] < heap[index]) {
-                swap(heap[parent], heap[index]);
-                index = parent;
-            } else {
-                break;
-            }
+    //inserting data in heap
+    void insert(int data){
+       size++;
+       int index=size;
+       arr[index]=data;
+       //giving correct position to the data
+       while(size>1){
+        int parent=index/2;
+        if(arr[parent]<arr[index]){
+            swap(arr[parent],arr[index]);
+            index=parent;
         }
+        else{
+            break;
+        }
+       }
     }
-
-    void printHeap() {
-        for (int i = 0; i < size; i++) {
-            cout << heap[i] << " ";
-        }
-        cout << endl;
+    void print(){
+        for(int i=1;i<size;i++){
+            cout<<arr[i]<<" ";
+        }cout<<endl;
     }
 };
-int main() {
-    int arr[] = {50, 55, 53, 52, 54};
-    int n = sizeof(arr) / sizeof(arr[0]);
-    MaxHeap h;
-    cout << "Inserting elements one by one...\n";
-    for (int i = 0; i < n; i++) {
-        h.insert(arr[i]);
-    }
-    cout << "Max-Heap after insertions: ";
-    h.printHeap();
-
-    return 0;
+int main(){
+ Heap h;
+ h.insert(55);
+ h.insert(15);
+ h.insert(59);
+ h.insert(58);
+ h.insert(59);
+ cout<<"Heap data  is"<<endl;
+ h.print();
 }
