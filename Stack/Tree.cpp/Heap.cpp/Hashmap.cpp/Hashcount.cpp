d0c819a0979264b90a067c9c4e7eedab9bb35b7f
Raja <kraj9380286@gmail.com>
2025-08-03 10:34:33 +0530

    commited

---
+++ Stack/Tree.cpp/Heap.cpp/Hashmap.cpp/Hashcount.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<unordered_map>
+using namespace std;
+int main(){
+  string str="Keshav";
+  unordered_map<char,int>map;
+  for(int i=0;i<str.length();i++){
+    char ch=str[i];
+     map[ch]++;
+  }
+  //printing the final output
+  for(auto i:map){
+    cout<<i.first<<"->"<<i.second<<endl;
+  }
+}
