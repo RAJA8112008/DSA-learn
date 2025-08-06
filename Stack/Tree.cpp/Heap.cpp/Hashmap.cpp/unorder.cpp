08e57c0584eb658d7d1b9dd20bb4d1eec69765f8
Raja <kraj9380286@gmail.com>
2025-08-06 22:33:27 +0530

    commited

--- Stack/Tree.cpp/Heap.cpp/Hashmap.cpp/unorder.cpp
+++ Stack/Tree.cpp/Heap.cpp/Hashmap.cpp/unorder.cpp
@@ -1,21 +1,8 @@
-#include<iostream>
-#include<unordered_map>
-using namespace std;
-int main(){
-    unordered_map<string,int>m;
-    //insertion in map
-    pair<string,int>p=make_pair("Rahul",8);
-    m.insert(p);
-    pair<string,int>p2=make_pair("Kumar",5);
-    m["Saniya"]=5;
-    //asses
-   cout<< m.at("Rahul")<<endl;
-   cout<<m["Saniya"]<<endl;
-   //search
-   
-   cout<<m.count("Saniya")<<endl;
-   for(auto i:m){
-    //becouse i is the pair of string and integer value so  
-    cout<<"->"<<i.first<<"->"<<i.second<<endl;
-   }
+#include <climits>
+#include <iostream>
+
+
+int main() {
+    std::cout << "INT_MAX = " << INT_MAX << std::endl;
+    return 0;
 }
