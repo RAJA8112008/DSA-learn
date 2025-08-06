3766d22000ebf16efd8ca4588f540a7bf04156e3
Raja <kraj9380286@gmail.com>
2025-08-06 22:37:39 +0530

    commited

--- Stack/Tree.cpp/Heap.cpp/Hashmap.cpp/unorder.cpp
+++ Stack/Tree.cpp/Heap.cpp/Hashmap.cpp/unorder.cpp
@@ -1,8 +1,37 @@
-#include <climits>
-#include <iostream>
+#include<iostream>
+#include<unordered_map>
+#include<string>
+using namespace std;
+class Solution {
+public:
+    int romanToInt(string s) {
+        // Step 1: Map Roman characters to integer values
+        unordered_map<char, int> roman = {
+            {'I', 1}, {'V', 5}, {'X', 10},
+            {'L', 50}, {'C', 100},
+            {'D', 500}, {'M', 1000}
+        };
 
+        int result = 0;
 
-int main() {
-    std::cout << "INT_MAX = " << INT_MAX << std::endl;
-    return 0;
+        // Step 2: Loop through the string
+        for (int i = 0; i < s.length(); i++) {
+            int curr = roman[s[i]];
+            int next = (i + 1 < s.length()) ? roman[s[i + 1]] : 0;
+
+            // Step 3: Apply rules (subtract if smaller before larger)
+            if (curr < next) {
+                result -= curr;
+            } else {
+                result += curr;
+            }
+        }
+
+        return result;
+    }
+};
+int main(){
+  int 
 }
+
+
