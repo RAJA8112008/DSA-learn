9c281c0b0042ae3eae95c7295afd4e9f6a18df0a
Raja <kraj9380286@gmail.com>
2025-08-07 20:07:16 +0530

    commited

--- Stack/Tree.cpp/Heap.cpp/Hashmap.cpp/unorder.cpp
+++ Stack/Tree.cpp/Heap.cpp/Hashmap.cpp/unorder.cpp
@@ -1,42 +1,34 @@
-#include<iostream>
-#include<unordered_map>
-#include<string>
+// Simple C++ program to print 
+// next smaller elements in a given array
+#include "bits/stdc++.h"
 using namespace std;
-class Solution {
-public:
-    int romanToInt(string s) {
-        // Step 1: Map Roman characters to integer values
-        unordered_map<char, int> roman = {
-            {'I', 1}, {'V', 5}, {'X', 10},
-            {'L', 50}, {'C', 100},
-            {'D', 500}, {'M', 1000}
-        };
 
-        int result = 0;
-
-        // Step 2: Loop through the string
-        for (int i = 0; i < s.length(); i++) {
-            int curr = roman[s[i]];
-            int next = (i + 1 < s.length()) ? roman[s[i + 1]] : 0;
-
-            // Step 3: Apply rules (subtract if smaller before larger)
-            if (curr < next) {
-                result -= curr;
-            } else {
-                result += curr;
+/* prints element and NSE pair 
+for all elements of arr[] of size n */
+void printNSE(int arr[], int n)
+{
+    int next, i, j;
+    for (i = 0; i < n; i++)
+    {
+        next = -1;
+        for (j = i + 1; j < n; j++)
+        {
+            if (arr[i] > arr[j])
+            {
+                next = arr[j];
+                break;
             }
         }
-
-        return result;
+        cout << arr[i] << " --> " 
+             << next << endl;
     }
-};
-int main(){
-    Solution sol;
-   string str;
-  cout<<"Enter the roman string"<<endl;
-   cin>>str;
-  int result=sol.romanToInt(str);
-  cout<<"Ans is:"<<result<<endl;
 }
 
-
+// Driver Code
+int main()
+{
+    int arr[]= {11, 13, 21, 3};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    printNSE(arr, n);
+    return 0;
+}
