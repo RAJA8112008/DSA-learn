9f0fbcfcf579361b1bd0d6c6ab8a791f25cff5dd
Raja <kraj9380286@gmail.com>
2025-08-07 22:27:55 +0530

    commited

--- Stack/Tree.cpp/Heap.cpp/Hashmap.cpp/Hashcount.cpp
+++ Stack/Tree.cpp/Heap.cpp/Hashmap.cpp/Hashcount.cpp
@@ -3,11 +3,13 @@
 using namespace std;
  bool check(vector<int>& val){
     int n=val.size();
+    int count=0;
     for(int i=0;i<n;i++){
-        if(val[i]>val[[i+1]%2]){
-            
+        if(val[i]>val[(i+1)%n]){
+            count++;
         }
     }
+    return count<=1;
  }
 int main(){
   vector<int>val={3,4,5,1,2};
