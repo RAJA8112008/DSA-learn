35bcc6d7281a4a04c0cd7aed2b87de39b36dd3e2
Raja <kraj9380286@gmail.com>
2025-08-07 22:26:13 +0530

    commited

--- Stack/Tree.cpp/Heap.cpp/Hashmap.cpp/Hashcount.cpp
+++ Stack/Tree.cpp/Heap.cpp/Hashmap.cpp/Hashcount.cpp
@@ -1,7 +1,16 @@
 #include<iostream>
 #include<vector>
 using namespace std;
+ bool check(vector<int>& val){
+    int n=val.size();
+    for(int i=0;i<n;i++){
+        if(val[i]>val[[i+1]%2]){
+            
+        }
+    }
+ }
 int main(){
   vector<int>val={3,4,5,1,2};
-  
+   check(val);
+
 }
