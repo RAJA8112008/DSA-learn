97976f59903b81f1b1afc08b60bb0138799bf2fa
Raja <kraj9380286@gmail.com>
2025-08-08 16:15:24 +0530

    commited

--- Stack/Tree.cpp/Heap.cpp/Hashmap.cpp/Hashcount.cpp
+++ Stack/Tree.cpp/Heap.cpp/Hashmap.cpp/Hashcount.cpp
@@ -1,22 +1,37 @@
-#include<iostream>
-#include<vector>
+#include <bits/stdc++.h>
 using namespace std;
- bool check(vector<int>& val){
-    int n=val.size();
-    int count=0;
-    for(int i=0;i<n;i++){
-        if(val[i]>val[(i+1)%n]){
-            count++;
+
+class Solution {
+public:
+    void reversePart(vector<int>& arr, int start, int end) {
+        while (start < end) {
+            swap(arr[start], arr[end]);
+            start++;
+            end--;
         }
     }
-    return count<=1;
- }
- 
-int main(){
-  vector<int>val={3,4,5,1,2};
-   if(check(val)){
-      cout<<"YES"<<endl;
-   }else{
-    cout<<"NO"<<endl;
-   }
+
+    void rotateLeft(vector<int>& arr, int d) {
+        int n = arr.size();
+        d = d % n; // In case d > n
+
+        // Step 1: Reverse first d elements
+        reversePart(arr, 0, d - 1);
+
+        // Step 2: Reverse remaining n-d elements
+        reversePart(arr, d, n - 1);
+
+        // Step 3: Reverse whole array
+        reversePart(arr, 0, n - 1);
+    }
+};
+
+int main() {
+    vector<int> arr = {1, 2, 3, 4, 5};
+    int d = 2;
+    Solution obj;
+    obj.rotateLeft(arr, d);
+
+    for (int x : arr) cout << x << " ";
+    return 0;
 }
