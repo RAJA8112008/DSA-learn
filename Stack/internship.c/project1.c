822e1593dd72786c85e728ad835e7f19246818d4
Raja <kraj9380286@gmail.com>
2025-08-10 10:21:58 +0530

    commited

--- Stack/internship.c/project1.c
+++ Stack/internship.c/project1.c
@@ -1,23 +1,42 @@
-#include<stdio.h>
-int main(){
-      int a[4][5] = {
+#include <stdio.h>
+
+int main() {
+    int a[4][5] = {
         {1, 1, 1, 1, 1}, // Employee 0
         {1, 0, 1, 1, 1}, // Employee 1
         {0, 0, 1, 1, 0}, // Employee 2
         {1, 1, 1, 1, 1}  // Employee 3
     };
-   char *dayName[] = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday"};
-     // Total attendance per employee
 
-    printf("Total attendance per employee");
-    for(int i=0;i<4;i++){
-        int sum=0;
-        for(int j=0;j<5;j++){
-            sum+=a[i][j];
-            printf("%d ",sum);
-             if (i < 3) printf(", ");
+    char *dayName[] = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday"};
+
+    // 1. Total attendance per employee
+    printf("Total attendance per employee: [");
+    for (int i = 0; i < 4; i++) {
+        int sum = 0;
+        for (int j = 0; j < 5; j++) {
+            sum += a[i][j];
+        }
+        printf("%d", sum);
+        if (i < 3) printf(", "); // comma between employees
+    }
+    printf("]\n");
+
+    // 2. Perfect attendance employees
+    printf("Perfect attendance: Employee [");
+    int firstPrinted = 0; // to handle commas
+    for (int i = 0; i < 4; i++) {
+        int sum = 0;
+        for (int j = 0; j < 5; j++) {
+            sum += a[i][j];
+        }
+        if (sum == 5) { // 5 means attended all days
+            if (firstPrinted) printf(", ");
+            printf("%d", i);
+            firstPrinted = 1;
         }
     }
+    printf("]\n");
 
-
+    return 0;
 }
