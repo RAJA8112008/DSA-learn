7c277012ccfbe654caf6e9c57513a67917861507
Raja <kraj9380286@gmail.com>
2025-08-29 21:42:34 +0530

    commited

--- Stack/Tree.cpp/Linked list.cpp/linked.cpp
+++ Stack/Tree.cpp/Linked list.cpp/linked.cpp
@@ -16,6 +16,10 @@ using namespace std;
     }
     void print(Node* &head){
         Node* temp=head;
+        while(temp!=NULL){
+            cout<<temp->data<<" "<<endl;
+            temp=temp->next;
+        }
     }
 int main(){
    Node* node1=new Node(10);
