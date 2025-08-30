e51c516fdae1799d624fe4e969cd32f4770b95f5
Raja <kraj9380286@gmail.com>
2025-08-30 23:07:47 +0530

    commited

--- Stack/Tree.cpp/Linked list.cpp/DLL.cpp
+++ Stack/Tree.cpp/Linked list.cpp/DLL.cpp
@@ -1,67 +1,94 @@
 #include<iostream>
 using namespace std;
-class Node{
-    public:
+
+class Node {
+public:
     int data;
     Node* prev;
     Node* next;
-    //constructor
-    Node(int data){
-        this->data=data;
-        this->prev=NULL;
-        this->next=NULL;
+    Node(int data) {
+        this->data = data;
+        this->prev = NULL;
+        this->next = NULL;
     }
 };
-//insert at position
-void insertAtposition(Node* &head,Node* &tail,int position,int data){
-    Node* temp=new Node(data);
-    //in starting
-    if(position==1){
-        insertAtHead(head,data);
+
+// insert at tail (move it before using)
+void insertAtTail(Node*& tail, int data) {
+    Node* temp = new Node(data);
+    if (tail != NULL) {
+        tail->next = temp;
+        temp->prev = tail;
+        tail = temp;
+    } else {
+        tail = temp;  // if tail is null, assign directly
+    }
+}
+
+// insert at head
+void insertAtHead(Node*& head, int data) {
+    Node* temp = new Node(data);
+    if (head != NULL) {
+        temp->next = head;
+        head->prev = temp;
+    }
+    head = temp;
+}
+
+// insert at position
+void insertAtposition(Node*& head, Node*& tail, int position, int data) {
+    if (position == 1) {
+        insertAtHead(head, data);
+        if (tail == NULL) tail = head; // if list was empty
         return;
     }
-    Node* temp=head;
-    int cnt=1;
-    while(cnt<position-1){
-       temp=temp->next;
-       cnt++;
-    }
-   
 
+    Node* temp = head;
+    int cnt = 1;
+    while (cnt < position - 1 && temp->next != NULL) {
+        temp = temp->next;
+        cnt++;
+    }
+
+    // insert at end if we're beyond the last node
+    if (temp->next == NULL) {
+        insertAtTail(tail, data);
+        return;
+    }
+
+    // insert in middle
+    Node* newnode = new Node(data);
+    newnode->next = temp->next;
+    temp->next->prev = newnode;
+    temp->next = newnode;
+    newnode->prev = temp;
 }
-//insert at head
-void insertAtHead(Node*&head,int data){
-   Node* temp=new Node(data);
-   temp->next=head;
-   
-   head->prev = temp;
-   head=temp;
+
+void print(Node*& head) {
+    Node* temp = head;
+    while (temp != NULL) {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
 }
-//insert at end
-void insertAtTail(Node* &tail,Node* &head,int data){
- Node* temp=new Node(data);
-  tail->next=temp;
-  temp->prev=tail;
-  tail=temp;
-}
-void print(Node*&head){
-    Node* temp=head;
-  while(temp!=NULL){
-    cout<<temp->data<<" ";
-    temp=temp->next;
-  }
-  cout<<endl;
-}
-int main(){
- Node* node1=new Node(10);
- Node* head=node1;
- Node* tail=node1;
-//  insertAtHead(head,15);
-//   insertAtHead(head,20);
-//   insertAtHead(head,20);
-//  print(head);
-  insertAtTail(tail,head,15);
-  insertAtTail(tail,head,20);
-  insertAtTail(tail,head,25);
- print(head);
+
+int main() {
+    Node* node1 = new Node(10);
+    Node* head = node1;
+    Node* tail = node1;
+
+    insertAtTail(tail, 15);
+    insertAtTail(tail, 20);
+    insertAtTail(tail, 25);
+    print(head);  
+
+    insertAtposition(head, tail, 2, 12);
+    print(head);  
+
+    insertAtposition(head, tail, 1, 5);
+    print(head);  
+
+    insertAtposition(head, tail, 10, 30);
+    print(head);  
 }
