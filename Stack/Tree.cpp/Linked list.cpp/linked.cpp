05c5ad9e5bf29913c4010d0ece4f9fcbba58afb7
Raja <kraj9380286@gmail.com>
2025-08-30 10:57:05 +0530

    commited

--- Stack/Tree.cpp/Linked list.cpp/linked.cpp
+++ Stack/Tree.cpp/Linked list.cpp/linked.cpp
@@ -9,12 +9,26 @@ class Node{
      this->point=NULL;
     }
 };
+//insert at tail
+void insertAttail(Node* &tail,int data){
+    Node* temp=new Node(data);
+    tail->point=temp;
+    tail=temp;
+}
 //insertion at head
  void insertAthead(Node* &head,int data){
     //creation of node
     Node* temp=new Node(data);
     temp->point=head;
     head=temp;
+ }
+ //print tail
+ void printTail(Node* tail){
+  while(tail!=NULL){
+    cout<<tail->data<<" ";
+    tail=tail->point;
+  }
+   cout<<endl;
  }
  //print the node
  void print(Node* head){
@@ -28,10 +42,17 @@ class Node{
 int main(){
  Node* node1=new Node(10);
  Node* head=node1;
- insertAthead(head,20);
- print(head);
- insertAthead(head,30);
-  print(head);
- insertAthead(head,40);
-   print(head);
+ Node* tail=node1;
+//  insertAthead(head,20);
+//  print(head);
+//  insertAthead(head,30);
+//   print(head);
+//  insertAthead(head,40);
+//    print(head);
+insertAttail(tail,20);
+printTail(tail);
+insertAttail(tail,30);
+printTail(tail);
+insertAttail(tail,40);
+printTail(tail);
 }
