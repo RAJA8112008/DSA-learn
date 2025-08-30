1498375bd4619a002854415e06bb3cca7f3a741e
Raja <kraj9380286@gmail.com>
2025-08-30 22:13:47 +0530

    commited

---
+++ Stack/Tree.cpp/Linked list.cpp/DLL.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node* prev;
+    Node* next;
+    //constructor
+    Node(int data){
+        this->data=data;
+        this->prev=NULL;
+        this->next=NULL;
+    }
+};
+void insertAtHead(Node*&head,int data){
+   Node* temp=new Node(data);
+   temp->next=head;
+   
+   head->prev = temp;
+   head=temp;
+}
+void print(Node*&head){
+    Node* temp=head;
+  while(temp!=NULL){
+    cout<<temp->data<<" ";
+    temp=temp->next;
+  }
+  cout<<endl;
+}
+int main(){
+ Node* node1=new Node(10);
+ Node* head=node1;
+ insertAtHead(head,15);
+  insertAtHead(head,20);
+ print(head);
+}
