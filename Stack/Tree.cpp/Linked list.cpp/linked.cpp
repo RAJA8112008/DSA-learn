7535afb126cacbdab8d7eb8f439c7e248da23af7
Raja <kraj9380286@gmail.com>
2025-08-31 00:08:36 +0530

    commited

--- Stack/Tree.cpp/Linked list.cpp/linked.cpp
+++ Stack/Tree.cpp/Linked list.cpp/linked.cpp
@@ -1,104 +1,34 @@
 #include<iostream>
 using namespace std;
-
 class Node{
-    public:
-    int data;
-    Node* point;
-    Node(int data){
-        this->data = data;
-        this->point = NULL;
-    }
+public:
+int data;
+Node* next;
+Node(int data){
+    this->data=data;
+    this->next=NULL;
+}
 };
-
-// insert at tail
-void insertAttail(Node* &tail,int data){
-    Node* temp = new Node(data);
-    tail->point = temp;
-    tail = temp;
-}
-
-// deletion of any node
-void deleteNode(int position, Node* &head) {
-    if(position == 1) {
-        Node* temp = head;
-        head = head->point;
-        temp->point = NULL;
-        delete temp;
-    }
-    else {
-        Node* prev = NULL;
-        Node* curr = head;
-        int cnt = 1;
-
-        while(cnt < position && curr != NULL) {
-            prev = curr;
-            curr = curr->point;
-            cnt++;
-        }
-
-        if(curr == NULL) return; // invalid position
-
-        prev->point = curr->point;
-        curr->point = NULL;
-        delete curr;
-    }
-}
-
-// insertion at head
 void insertAthead(Node* &head,int data){
-    Node* temp = new Node(data);
-    temp->point = head;
-    head = temp;
+  Node* temp=new Node(data);
+  temp->next=head;
+  head=temp;
 }
-
-// insert at position
-void insertAtposition(Node* &head, Node* &tail, int position, int data){
-    if(position == 1){
-        insertAthead(head,data);
-        return;
+void print(Node*head){
+    Node* temp=head;
+    while(temp!=NULL){
+        cout<<temp->data<<" ";
+        temp=temp->next;
     }
-    Node* temp = head;
-    int cnt = 1;
-    while(cnt < position-1){
-        temp = temp->point;
-        cnt++;
-    }
-    if(temp->point == NULL){
-        insertAttail(tail,data);
-        return;
-    }
-    Node* node = new Node(data);
-    node->point = temp->point;
-    temp->point = node;
+    cout<<endl;
 }
-
-// print linked list
-void print(Node* head){
-    while(head != NULL){
-        cout << head->data << " ";
-        head = head->point;
-    }
-    cout << endl;
-}
-
 int main(){
-    Node* node1 = new Node(10);
-    Node* head = node1;
-    Node* tail = node1;
-
-    insertAttail(tail,20);
-    print(head);
-    insertAttail(tail,30);
-    print(head);
-    insertAttail(tail,40);
-    print(head);
-    insertAtposition(head,tail,3,50);
-    print(head);
-
-    deleteNode(5,head);   
-    print(head);
-
-    cout << "Head " << head->data << endl;
-    cout << "Tail " << tail->data << endl;
+Node* node1=new Node(15);
+Node* head=node1;
+insertAthead(head,20);
+print(head);
+insertAthead(head,55);
+print(head);
+insertAthead(head,65);
+print(head);
 }
