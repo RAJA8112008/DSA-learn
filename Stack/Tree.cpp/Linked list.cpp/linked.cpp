a435e5ac060a5c0ef71604871c62a360bbea742a
Raja <kraj9380286@gmail.com>
2025-08-30 10:38:54 +0530

    commited

--- Stack/Tree.cpp/Linked list.cpp/linked.cpp
+++ Stack/Tree.cpp/Linked list.cpp/linked.cpp
@@ -1,33 +1,30 @@
 #include<iostream>
 using namespace std;
- class Node{
-        public:
-        int data;
-        Node* next;
-        Node(int data){
-            this->data=data;
-            this->next=NULL;
-        }
-    };
-    void insertAtNode(Node* &head,int data){
-        Node* temp=new Node(data);
-        temp->data=data;
-        head=temp;
+class Node{
+    public:
+    int data;
+    Node* point;
+    Node(int data){
+     this->data=data;
+     this->point=NULL;
     }
-    void print(Node* &head){
-        Node* temp=head;
-        while(temp!=NULL){
-            cout<<temp->data<<" "<<endl;
-            temp=temp->next;
-        }
-    }
+};
+//insertion at head
+ void insertAthead(Node* &head,int data){
+    //creation of node
+    Node* temp=new Node(data);
+    temp->point=head;
+    head=temp;
+ }
+ //print the node
+ void print(Node* head){
+    
+   while(head!=NULL){
+    cout<<head->data<<" ";
+    head=head->point;
+   }
+ }
 int main(){
-   Node* node1=new Node(10);
-Node* head=node1;
-  insertAtNode(head,12);
-  print(head);
-  insertAtNode(head,15);
-  print(head);
-  insertAtNode(head,19);
-  print(head);
+ Node* node1=new Node(10);
+ Node* head=node1;
 }
