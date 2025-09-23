ac9a515ffe18735438ca709e1759dbd1924998b3
Raja <kraj9380286@gmail.com>
2025-09-23 21:33:10 +0530

    commited

--- Stack/Tree.cpp/Linked list.cpp/linked.cpp
+++ Stack/Tree.cpp/Linked list.cpp/linked.cpp
@@ -4,6 +4,8 @@ using namespace std;
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+
+        
         // Using HashMap approach for O(n) solution
         unordered_map<int,int> mp;  // value -> index
         for (int i = 0; i < nums.size(); i++) {
