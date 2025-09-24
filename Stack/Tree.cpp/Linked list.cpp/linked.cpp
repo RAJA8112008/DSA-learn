c038f5ca9ea373043fb15d293f6eaf7038d912e7
Raja <kraj9380286@gmail.com>
2025-09-24 21:22:41 +0530

    commited

--- Stack/Tree.cpp/Linked list.cpp/linked.cpp
+++ Stack/Tree.cpp/Linked list.cpp/linked.cpp
@@ -3,6 +3,12 @@ using namespace std;
 
 class Solution {
 public:
+
+
+
+
+
+
     vector<int> twoSum(vector<int>& nums, int target) {
         unordered_map<int,int> mp;  // value -> index
         for (int i = 0; i < nums.size(); i++) {
