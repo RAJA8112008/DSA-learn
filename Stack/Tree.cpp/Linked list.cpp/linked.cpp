82da9b0bf0549b942aeb163730bd76a7a5b6fbf3
Raja <kraj9380286@gmail.com>
2025-09-24 21:22:20 +0530

    commited

--- Stack/Tree.cpp/Linked list.cpp/linked.cpp
+++ Stack/Tree.cpp/Linked list.cpp/linked.cpp
@@ -4,7 +4,6 @@ using namespace std;
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-  
         unordered_map<int,int> mp;  // value -> index
         for (int i = 0; i < nums.size(); i++) {
             int complement = target - nums[i];
