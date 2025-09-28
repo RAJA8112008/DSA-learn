fef7de1755685cd9f5d731859576a11d9eb9b208
Raja <kraj9380286@gmail.com>
2025-09-28 16:58:30 +0530

    commited

--- Stack/Tree.cpp/Linked list.cpp/linked.cpp
+++ Stack/Tree.cpp/Linked list.cpp/linked.cpp
@@ -1,16 +1,13 @@
 #include<stdio.h>
 #include<stdlib.h>
-  int fact(int num){
-   //base condition
-   if(num==0 ||num==1){
-      return 1;
-   }
-   return (num)*fact(num-1);
-  }
-int main(){
+  int main(){
    int num;
-   printf("Enter the number");
+   long  long fact=1;
+   printf("Enter the numbers:");
    scanf("%d",&num);
-   // fact(num);
-   printf("%d",fact(num));
-}
+   for(int i=1;i<=num;i++){
+   fact=fact*i;
+   }
+   printf("%d",fact);
+
+  }
