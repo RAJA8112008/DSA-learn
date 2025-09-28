72ddbab7cd76893704898ea1548aa6b0deade2c1
Raja <kraj9380286@gmail.com>
2025-09-28 11:59:49 +0530

    commited

--- Stack/Tree.cpp/Linked list.cpp/linked.cpp
+++ Stack/Tree.cpp/Linked list.cpp/linked.cpp
@@ -1,111 +1,36 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-// Define node
-struct Node {
+#include<stdio.h>
+#include<stdlib.h>
+ struct Node{
     int data;
-    struct Node* prev;
     struct Node* next;
-};
-
-// Insert at end (for creating DLL easily)
-void insertAtEnd(struct Node** head, int value) {
-    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
-    newNode->data = value;
-    newNode->next = NULL;
-
-    if (*head == NULL) {
-        newNode->prev = NULL;
-        *head = newNode;
-        return;
-    }
-
-    struct Node* temp = *head;
-    while (temp->next != NULL) {
-        temp = temp->next;
-    }
-
-    temp->next = newNode;
-    newNode->prev = temp;
-}
-
-   void deleteAtpos(struct Node** head,int pos){
-     struct Node* temp=*head;
-     int count=1;
-     while(temp!=NULL && count<pos){
-        temp=temp->next;
-        count++;
+ };
+  //creation of LL 
+struct Node creatCLL(int n){
+     if(n<=0) return NULL;
+     //creation of Nodes
+     struct Node* nodes[n];
+     for(int i=0;i<n;i++){
+      nodes[i]=(struct Node*)malloc(sizeof(struct Node));
+      printf("Enter the data for %d",i+1);
+      scanf("%d",&nodes[i]);
      }
-          temp->prev->next=temp->next;
-          temp->next=temp->prev;
-         
-   }
-// Delete at beginning
-// void deleteAtBeginning(struct Node** head) {
-//     if (*head == NULL) {
-//         printf("DLL is empty, nothing to delete!\n");
-//         return;
-//     }
-//     struct Node* temp=*head;
-//     if(temp->next!=NULL){
-//         (*head)=temp->next;
-//     }
-//     (*head)->prev=NULL;
-//     free(temp);
-// }
-//     struct Node* temp = *head;
-
-//     *head = temp->next;  // move head to next node
-//     if (*head != NULL) {
-//         (*head)->prev = NULL;
-//     }
-
-//     printf("Deleted element: %d\n", temp->data);
-//     free(temp);
-// }
-  void deleteAtend(struct Node**head){
-    struct Node* temp=*head;
-    while(temp->next!=NULL){
-            temp=temp->next;
-    }
-    temp->prev->next=NULL;
-    temp->prev=NULL;
-    free(temp);
+     //circular
+     for(int i=0;i<n;i++){
+        nodes[i]->next=nodes[(i+1)%n];
+     }
+     return nodes[0];
   }
 
-// Display DLL
-void display(struct Node* head) {
-    struct Node* temp = head;
-    printf("DLL: ");
-    while (temp != NULL) {
-        printf("%d <-> ", temp->data);
-        temp = temp->next;
-    }
-    printf("NULL\n");
-}
-
-int main() {
-    struct Node* head = NULL;
-
-    // Create DLL
-    insertAtEnd(&head, 10);
-    insertAtEnd(&head, 20);
-    insertAtEnd(&head, 30);
-
-    display(head);
-   deleteAtpos(&head,2);
-    display(head);
-    // Delete nodes from beginning
-//    deleteAtend(&head);
-//     display(head);
-
-//     deleteAtend(&head);
-//     display(head);
-
-//    deleteAtend(&head);
-//     display(head);
-
-//     deleteAtend(&head);  // trying again when empty
-
-    return 0;
+  void display(struct Node* head){
+     if(head==NULL){
+       printf("Not NODES available");
+     }
+  }
+int main(){
+    //number of nodes
+    int n;
+    printf("No. of nodes");
+    scanf("%d",&n);
+    struct Node* head=creatCLL(n);
+      display(head);
 }
