7cba5fb1576cc91874454b13e913d6b305985274
Raja <kraj9380286@gmail.com>
2025-09-29 11:19:10 +0530

    commited

--- Stack/Tree.cpp/Linked list.cpp/linked.cpp
+++ Stack/Tree.cpp/Linked list.cpp/linked.cpp
@@ -1,46 +1,38 @@
 #include<stdio.h>
 #include<stdlib.h>
+//doubly linked list
 struct node{
-  int  data;
+  int data;
   struct node* next;
+  struct node* prev;
 };
-void insertAthead(struct node** head,int data){
+void innsertAthead(struct node**head,int val){
   struct node* newnode=(struct node*)malloc(sizeof(struct node));
-    newnode->data=data;
-    newnode->next=*head;
-    *head=newnode;
+  newnode->prev=NULL;
+  newnode->data=val;
+  newnode->next=*head;
+  if(head!=NULL){
+  (*head)->prev=newnode;
   }
-  //insert at end
-  void insertAtend(struct node**head,int data){
+  *head=newnode;
+}
+void insertattail(struct node* head,int val){
    struct node* newnode=(struct node*)malloc(sizeof(struct node));
-   newnode->data=data;
-    newnode->next=NULL;
-    if(*head==NULL){
-      *head=newnode;
-      return;
-    }
-    struct node* temp=*head;
-      while(temp->next!=NULL){
-        temp=temp->next;
-        temp->next=newnode;
-      }
-  }
-  //insert at position
-  void insertAtpos(struct node** head,int  val,int pos){
-    struct node* newnode=(struct node*)malloc(sizeof(struct node));
-     newnode->data=val;
-    if(pos==1){
-      newnode->next=*head;
-      *head=newnode;
-      return;
-    }
-    //insertion at any pos
-    struct node* temp=*head;
-    int count=1;
-    while(temp->next!=NULL && count<pos){
-      temp=temp->next;
-    }
-  }
+   newnode->data=val;
+   newnode->next=NULL;
+   newnode->prev=NULL;
+   if(*head==NULL){
+    *head=newnode;
+    return;
+   }
+   struct node* temp=head;
+   while(temp->next!=NULL){
+     temp=temp->next;
+   }
+   temp->next=newnode;
+   newnode->prev=temp;
+
+}
 int main(){
-  struct node* head=NULL;
+ struct node* head=NULL;
 }
