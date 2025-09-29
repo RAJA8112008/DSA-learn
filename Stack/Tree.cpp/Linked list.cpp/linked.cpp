35d0dbb08bf2e9c316367cb1877ef75dc318cc7c
Raja <kraj9380286@gmail.com>
2025-09-29 09:43:24 +0530

    commited

--- Stack/Tree.cpp/Linked list.cpp/linked.cpp
+++ Stack/Tree.cpp/Linked list.cpp/linked.cpp
@@ -1,18 +1,45 @@
 #include<stdio.h>
 #include<stdlib.h>
-int tailRecursion(int num,int result){
-    if(num==0 ||num==1){
-      return result;
+#define MAX 5
+ int arr[MAX];
+ int top=-1;
+ //check stack is empty  or not
+ int isempty(){
+     return top==-1;
+ }
+ 
+ int isfull(){
+     return top==MAX-1;
+ }
+ void  push(int data){
+    //check stack is full or not
+    if(isfull()){
+      printf("Overflow");
     }
-    return tailRecursion(num-1,num*result);
-
-}
-  int main(){
-   //tail recursion
-  int num;
-  int result=1;
-  printf("Enter the num:");
-  scanf("%d",&num);
-  result= tailRecursion(num,result);
-   printf("%d",result);
+     top++;
+    arr[top]=data;
+ }
+ //pop function 
+ int pop(){
+  if(isempty()){
+    printf("Stack  is empty");
   }
+  return arr[top--];
+ }
+ //to display the overall  stack
+ int display(){
+  if(isempty()){
+    printf("Stack is empty");
+  }
+  for(int i=0;i<=top;i++){
+       printf("%d",arr[i]);
+  }
+ };
+ //check stack is full or not
+int main(){
+  push(10);
+  push(20);
+  push(30);
+  push(40);
+display()
+}
