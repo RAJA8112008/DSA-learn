80fef1b0a300a1be977b9010e7154afbcb81ed5d
Raja <kraj9380286@gmail.com>
2025-09-29 11:31:06 +0530

    commited

--- Stack/Tree.cpp/Linked list.cpp/linked.cpp
+++ Stack/Tree.cpp/Linked list.cpp/linked.cpp
@@ -1,38 +1,24 @@
-#include<stdio.h>
 #include<stdlib.h>
-//doubly linked list
+#include<stdlib.h>
 struct node{
   int data;
   struct node* next;
-  struct node* prev;
 };
-void innsertAthead(struct node**head,int val){
-  struct node* newnode=(struct node*)malloc(sizeof(struct node));
-  newnode->prev=NULL;
-  newnode->data=val;
-  newnode->next=*head;
-  if(head!=NULL){
-  (*head)->prev=newnode;
-  }
-  *head=newnode;
-}
-void insertattail(struct node* head,int val){
-   struct node* newnode=(struct node*)malloc(sizeof(struct node));
-   newnode->data=val;
-   newnode->next=NULL;
-   newnode->prev=NULL;
-   if(*head==NULL){
+//ccircular linked list
+void insertAtEnd(struct node* head,int data){
+    struct node* newnode=(struct node*)malloc(sizeof(struct node));
+  newnode->data=data;
+  newnode->next=NULL;
+  if(*head==NULL){
     *head=newnode;
-    return;
-   }
-   struct node* temp=head;
-   while(temp->next!=NULL){
-     temp=temp->next;
+  }
+  struct node*temp=head;
+   while(temp->next!=head){
+    temp=temp->next;
    }
    temp->next=newnode;
-   newnode->prev=temp;
-
+   newnode->next=*head;
 }
 int main(){
- struct node* head=NULL;
+  struct node* head=NULL;
 }
