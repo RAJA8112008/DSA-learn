8a27eb05b917f52aea2356b5a99d57872d5c1430
Raja <kraj9380286@gmail.com>
2025-10-09 22:25:34 +0530

    commited

--- Stack/Tree.cpp/Heap.cpp/Insert.cpp
+++ Stack/Tree.cpp/Heap.cpp/Insert.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main(){
     //sum of n natural numbers  given  by the user
-    int n,i;
+    int n,i=0;
     cout<<"Enter the number:"<<endl;
     cin>>n;
    while(i<=n){
