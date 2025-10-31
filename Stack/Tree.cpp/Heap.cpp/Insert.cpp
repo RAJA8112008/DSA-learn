5e410b6a12bb74d7556af579c83d4f72bb2688a0
Raja <kraj9380286@gmail.com>
2025-10-31 18:40:10 +0530

    commited

--- Stack/Tree.cpp/Heap.cpp/Insert.cpp
+++ Stack/Tree.cpp/Heap.cpp/Insert.cpp
@@ -1,12 +1,57 @@
-#include<iostream>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
-    //sum of n natural numbers  given  by the user
-    int n,i=0;
-    cout<<"Enter the number:"<<endl;
-    cin>>n;
-   while(i<=n){
-    cout<<i<<" ";
-    i++;
-   }cout<<endl;
+
+bool dfs(int node, int parent, vector<int> adj[], vector<int> &vis) {
+    vis[node] = 1;
+    for (auto it : adj[node]) {
+        if (!vis[it]) {
+            if (dfs(it, node, adj, vis)) 
+                return true; // cycle found
+        }
+        else if (it != parent) 
+            return true; // back edge => cycle
+    }
+    return false;
+}
+
+bool isTree(int V, vector<int> adj[]) {
+    vector<int> vis(V, 0);
+
+    // Step 1: Check cycle
+    if (dfs(0, -1, adj, vis)) return false;
+
+    // Step 2: Check connectedness
+    for (int i = 0; i < V; i++) {
+        if (!vis[i]) return false;
+    }
+
+    // Step 3: Tree must have V-1 edges (can be checked outside)
+    return true;
+}
+
+int main() {
+    int V = 4; // vertices
+    int E = 3; // edges
+    vector<int> adj[V];
+
+    // edges input
+    adj[0].push_back(1);
+    adj[1].push_back(0);
+    adj[1].push_back(2);
+    adj[2].push_back(1);
+    adj[2].push_back(3);
+    adj[3].push_back(2);
+
+    // Edge condition
+    if (E != V - 1) {
+        cout << "Not a Tree";
+        return 0;
+    }
+
+    if (isTree(V, adj))
+        cout << "Graph is a Tree";
+    else
+        cout << "Graph is NOT a Tree";
+
+    return 0;
 }
